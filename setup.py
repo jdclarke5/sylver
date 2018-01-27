4436fa9113cca129fed8b02d6458c78e268bf4ec
Unknown <jdclarke5@gmail.com>
2018-01-27 16:25:16 +1100

    Initial structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='sylver',
+    version='0.1',
+    description='Fun with Conway\'s game of Sylver Coinage',
+    url='http://github.com/jdclarke5/sylver/',
+    author='Jackson Clarke',
+    license='MIT',
+    packages=find_packages(),
+    )
+    
