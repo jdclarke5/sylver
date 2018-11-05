44736666ead46ffdc7e1ae60ac6fa6a815586839
Jackson <jdclarke5@gmail.com>
2018-11-05 21:06:32 +1100

    method for finding winners

--- sylver/search.py
+++ sylver/search.py
@@ -18,21 +18,20 @@ def get_children(position):
         _pos.add(g)
         children.append(_pos)
 
-def generate_descendant_tree(seeds, length):
+def generate_descendant_tree(seeds, length, verbose=False):
     """
-    Generates the full ancestor tree.
+    Generates the full game graph from a seed.
     """
     # Start the game to track history
-    game = Game(seeds, length)
+    game = Game(seeds, length, verbose=verbose)
     # Initialise the directed graph
     graph = nx.DiGraph()
     _pos = copy.deepcopy(game.state)
     seed = repr(game.state)
-    graph.add_node(seed, object=_pos)
+    graph.add_node(seed, position=_pos)
     # Recursively traverse the graph and add nodes/edges
     def _traverse_gaps(parent):
         for g in game.state.gaps:
-            print("TRAVERSE: Adding {} to {}".format(g, parent))
             # Play an available move
             game.play(g)
             # Add this node/edge to graph
@@ -43,7 +42,7 @@ def generate_descendant_tree(seeds, length):
                 graph.add_edge(parent, name)
             # Otherwise add node, edge, and continue traverse
             else:
-                graph.add_node(name, object=_pos)
+                graph.add_node(name, position=_pos)
                 graph.add_edge(parent, name)
                 _traverse_gaps(name)
             # We are done with this play
@@ -52,5 +51,44 @@ def generate_descendant_tree(seeds, length):
     _traverse_gaps(seed)
     return graph
 
-    
-
+def determine_winners(graph):
+    """
+    Finds P/N status of nodes from a full game graph.
+    """
+    # This algorithm traverses the graph multiple, each time
+    # filling in what it can, until it is done.
+    def _traverse():
+        complete = True
+        for name, data in graph.nodes.items():
+            # Skip if already known
+            if data.get("winner"):
+                continue
+            complete = False
+            # Set irreducibles (except [2,3]) to N, including [1]
+            _pos = data["position"]
+            if (_pos.is_irreducible == True and _pos.generators.tolist() != [2, 3]):
+                data["winner"] = "N"
+                print("N (irreducible) : {}".format(repr(_pos)))
+                continue
+            # If unknown has one child P, then it's N.
+            # If unknown has all children N, then it's P.
+            for child in graph.successors(name):
+                if graph.nodes[child].get("winner") == "P":
+                    data["winner"] = "N"
+                    print("N (child is P)  : {}".format(repr(_pos)))
+                    break
+                if graph.nodes[child].get("winner") != "N":
+                    break
+            else:
+                # No break, so must be P
+                data["winner"] = "P"
+                print("P (child all N) : {}".format(repr(_pos)))
+        return complete
+    # Loop until we are complete
+    iter = 1
+    while True:
+        print("Traverse iteration: {}".format(iter))
+        if _traverse():
+            break
+        iter = iter + 1
+         
